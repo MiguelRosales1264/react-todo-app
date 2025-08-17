1bb0875caaab7e0e5e8514a7773fa3c7c4ec639c
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-08-17 07:15:28 -0600

    remove .env from config.js

--- task-flow/src/firebase/config.js
+++ task-flow/src/firebase/config.js
@@ -2,16 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { apiKey, authDomain, projectId } from '../.env'; // Adjust the import path as needed
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    projectId: projectId,
+    apiKey: "AIzaSyB2Q2TCL45GKTyHJjqeXyaaytU91JaqQBE",
+    authDomain: "taskflow-app-28b78.firebaseapp.com",
+    projectId: "taskflow-app-28b78",
     storageBucket: "taskflow-app-28b78.firebasestorage.app",
     messagingSenderId: "693275204267",
     appId: "1:693275204267:web:9c0207f1508dc835e3b5ee"
