98f7aa19815e03e19f726358fbcf2bb722ab6480
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-08-16 19:11:11 -0600

    basic page layout

--- task-flow/src/App.jsx
+++ task-flow/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
 
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       <Header />
+
+      <main className='flex-grow flex items-center justify-center p-4'>
+        <h1 className='text-center text-3xl font-bold text-blue-600'>Welcome to TaskFlow</h1>
+      </main>
+
+      <Footer />
     </div>
   )
 }
--- task-flow/src/components/Header.jsx
+++ task-flow/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
 return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-blue-600 text-white p-4">
         <h1 className="text-2xl font-bold">TaskFlow</h1>
     </header>
 );
